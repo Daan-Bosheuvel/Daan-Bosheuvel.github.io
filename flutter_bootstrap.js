446b8e112837e3e65cf8db920f0ab39ff434ef8c
Daan-Bosheuvel <danavanhassel@gmail.com>
2024-11-19 14:25:43 +0100

    added first API with wem

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3239084645"
+    serviceWorkerVersion: "2492707302"
   }
 });
